0aad4d3e2193c540537d8539dd505ee0527ef66a
Max Arouca <maximiler@gmail.com>
2018-04-19 14:03:53 -0300

    Adding validation on the front end

--- models/seriesModel.js
+++ models/seriesModel.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const SerieScheema = mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    // required: true
+  },
   status: {
     type: String,
     enumValues: ['to-watch', 'watched', 'watching']
