a5a7125558afc27b15d9cde5928fd7b49484d8ff
Max Arouca <maximiler@gmail.com>
2018-04-19 09:38:46 -0300

    Adding Update and Removing in CRUD

--- controllers/seriesController.js
+++ controllers/seriesController.js
@@ -1,21 +1,60 @@
-const Serie = require('../models/seriesModel')
+const labels = [{
+    id: 'watched',
+    name: 'Assistida'
+  },
+  {
+    id: 'to-watch',
+    name: 'Para Assistir'
+  },
+  {
+    id: 'watching',
+    name: 'Assistindo'
+  }
+]
 
-const indexSerie = (req, res) => {
-  
+const indexSerie = ({ Serie }, req, res) => {
   Serie.find({}, (err, docs) => {
-    res.render('series/index', { series: docs })
+    res.render('series/index', { series: docs, labels })
   })
 }
 
 const newSerie = (req, res) => {
-  const serie = new Serie({
-    name: 'Chicago Med',
-    status: 'watching'
-  })
-  serie.save(() => console.log('saved'))
   res.render('series/new')
 }
 
+const newSerieProcess = ({ Serie }, req, res) => {
+  const serie = new Serie(req.body)
+  serie.save(() => res.redirect('/series'))
+}
+
+const updateSerie = ({ Serie }, req, res) => {  
+  Serie.findOne({
+    _id: req.params.id
+  }, (err, serie) => {
+    res.render('series/update', { serie, labels })
+  })
+}
+
+const updateSerieProcess = ({ Serie }, req, res) => {
+
+  Serie.findOne({
+    _id: req.params.id
+  },
+  (err, serie) => {
+    serie.name = req.body.name
+    serie.status = req.body.status
+    serie.save()
+    res.redirect('/series')
+  })
+}
+
+const deleteSerie = ({ Serie }, req, res) => {
+  Serie.remove({
+    _id: req.params.id
+  }, () => res.redirect('/series'))
+}
+
+
 module.exports = {
-  indexSerie, newSerie
+  indexSerie, newSerie, newSerieProcess, updateSerie, updateSerieProcess, deleteSerie
 }
--- routes/seriesRoutes.js
+++ routes/seriesRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const seriesController = require('../controllers/seriesController')
+const Serie = require('../models/seriesModel')
 
-router.get('/', seriesController.indexSerie)
+const models = { Serie }
 
+router.get('/', seriesController.indexSerie.bind(null, models))
 router.get('/new', seriesController.newSerie)
+router.post('/new', seriesController.newSerieProcess.bind(null, models))
+router.get('/update/:id', seriesController.updateSerie.bind(null, models))
+router.post('/update/:id', seriesController.updateSerieProcess.bind(null, models))
+router.get('/delete/:id', seriesController.deleteSerie.bind(null, models))
 
 
 module.exports = router
